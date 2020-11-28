c42414587357f97fd8c3e66c5a63a70c5a9a7342
nemoaleexey <72552666+nemoaleexey@users.noreply.github.com>
2020-11-28 20:42:50 +0600

    form

--- Portfolio/dist/mail.php
+++ Portfolio/dist/mail.php
@@ -6,20 +6,20 @@ $mail->CharSet = 'utf-8';
 
 $name = $_POST['user_name'];
 $phone = $_POST['user_phone'];
-$text = $_POST['user_text'];
+
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'alexeyport@bk.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Username = 'alesha.b.0002@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('alexeyport@bk.ru'); // от кого будет уходить письмо?
-$mail->addAddress('alesha.b.0002@bk.ru');     // Кому будет уходить письмо 
+$mail->setFrom('alesha.b.0002@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('alesha.b.0002@gmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
