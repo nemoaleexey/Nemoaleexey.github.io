242bebec38983586d55e8f12242fa57c8ea10310
nemoaleexey <72552666+nemoaleexey@users.noreply.github.com>
2020-11-29 07:03:41 +0600

    -

--- Photoshop/dist/js/script.js
+++
@@ -1,5 +0,0 @@
-$("a[href^='#']").click(function() { // Это универсальный плавный скролл наверх, так же подойдет для меню.
-    const _href = $(this).attr("href");
-    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" }, 1000);
-    return false;
-});
